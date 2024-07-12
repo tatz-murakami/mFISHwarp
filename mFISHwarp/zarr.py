1e96ece295ac3bf59c37b4e969ac80744cc4dee0
tmurakami <cunshang.m.tatsuya@gmail.com>
2024-07-12 15:01:12 -0400

    add functions in zarr module

--- mFISHwarp/zarr.py
+++ mFISHwarp/zarr.py
@@ -1,5 +1,6 @@
 from ome_zarr.scale import Scaler
 from ome_zarr.dask_utils import resize as dask_resize
+from ome_zarr.dask_utils import downscale_nearest
 import dask.array as da
 import numpy as np
 # to enable three dimensional scaler, I have to define my own class by inheriting the Scaler class
@@ -34,3 +35,53 @@ class IsoScaler(Scaler):
             image.astype(float), out_shape, order=1, mode="reflect", anti_aliasing=False
         )
         return image.astype(dtype)
+
+
+def datasets_metadata_generator(physical_scale, downscale_factor=(1, 2, 2, 2), pyramid_level=1):
+    """
+    physical_scale (list or tuple): same size as the dimension of the image. if CZYX, it should have size of 4.
+    downscale_factor (list or tuple): same size as the data_set_generator. it will be converted to int if it is not.
+    """
+    if len(physical_scale) != len(downscale_factor):
+        raise ValueError("The size of physical scale should match with the size of downscale factor")
+
+    downscale_factor = tuple(int(i) for i in downscale_factor)
+
+    datasets = []
+    for resolution in range(pyramid_level):
+        scale = [i * (j ** resolution) for i, j in zip(physical_scale, downscale_factor)]
+        datasets.append(
+            {"path": str(int(resolution)),
+             "coordinateTransformations": [
+                 {
+                     "scale": scale,
+                     "type": "scale"
+                 }
+             ]}
+        )
+
+    return datasets
+
+
+def pyramid_generator_from_dask(arr, downscale_factor=(1, 2, 2, 2), pyramid_level=5, chunk=(1, 256, 256, 256)):
+    """
+    Nearest neighbor downsampling to make pyramid format.
+    arr (da.ndarray)
+    downscale_factor (tuple or list): the size of the downscale_factor should be same as the dimension of arr.
+    it will be converted to int if it is not.
+    pyramid_level (int)
+    """
+    if arr.ndim != len(downscale_factor):
+        raise ValueError("The size of downscale_factor should match with the dimension of arr")
+    downscale_factor = tuple(int(i) for i in downscale_factor)
+
+    if arr.ndim != len(chunk):
+        raise ValueError("The size of chunk should match with the dimension of arr")
+
+    pyramid_arr = []
+    for resolution in range(pyramid_level):
+        factors = tuple(i ** resolution for i in downscale_factor)
+        pyramid_arr.append(da.rechunk(downscale_nearest(arr, factors), chunk))
+
+    return pyramid_arr
+
